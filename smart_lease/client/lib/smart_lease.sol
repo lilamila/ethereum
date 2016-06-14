ab3edb35113dcf6132805345752370d883736eb3
mleafer <marieleaf@Maries-MacBook-Air-2.local>
2016-06-14 11:06:56 -0400

    move doc

--- smart_lease/client/lib/smart_lease.sol
+++ smart_lease/client/lib/smart_lease.sol
@@ -57,7 +57,7 @@ contract SmartLease  {
         tokenReimbursement.transfer(msg.sender, amount/price)
         StartLease(msg.sender, Landlord(), now)
     }
-    
+
     /* lease signing */
     function signLease(string _signature) public {
         signature = _signature;
@@ -83,64 +83,4 @@ contract SmartLease  {
 
 
 contract token { function transfer(address receiver, uint amount){  } }
-
-
-contract smartLease {
-    address public beneficiary;
-    uint public fundingGoal; uint public amountRaised; uint public deadline; uint public price;
-    token public tokenReward;   
-    Funder[] public funders;
-    event FundTransfer(address backer, uint amount, bool isContribution);
-    bool smartLeaseClosed = false;
-
-    /* data structure to hold information about campaign contributors */
-    struct Funder {
-        address addr;
-        uint amount;
-    }
-
-    /*  at initialization, setup the owner */
-    function smartLease(
-        address ifSuccessfulSendTo, 
-        uint fundingGoalInEthers, 
-        uint durationInMinutes, 
-        uint etherCostOfEachToken, 
-        token addressOfTokenUsedAsReward
-    ) {
-        beneficiary = ifSuccessfulSendTo;
-        fundingGoal = fundingGoalInEthers * 1 ether;
-        deadline = now + durationInMinutes * 1 minutes;
-        price = etherCostOfEachToken * 1 ether;
-        tokenReward = token(addressOfTokenUsedAsReward);
-    }   
-
-    /* The function without name is the default function that is called whenever anyone sends funds to a contract */
-    function () {
-        if (smartLeaseClosed) throw;
-        uint amount = msg.value;
-        funders[funders.length++] = Funder({addr: msg.sender, amount: amount});
-        amountRaised += amount;
-        tokenReward.transfer(msg.sender, amount / price);
-        FundTransfer(msg.sender, amount, true);
-    }
-
-    modifier afterDeadline() { if (now >= deadline) _ }
-
-    /* checks if the goal or time limit has been reached and ends the campaign */
-    function checkGoalReached() afterDeadline {
-        if (amountRaised >= fundingGoal){
-            beneficiary.send(amountRaised);
-            FundTransfer(beneficiary, amountRaised, false);
-        } else {
-            for (uint i = 0; i < funders.length; ++i) {
-              funders[i].addr.send(funders[i].amount);  
-              FundTransfer(funders[i].addr, funders[i].amount, false);
-            }               
-        }
-
-        beneficiary.send(this.balance); // send any remaining balance to beneficiary anyway
-        smartLeaseClosed = true;
-    }
-}
-
 
