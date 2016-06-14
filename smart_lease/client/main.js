60318096f7cc050e38ef7ee4d29321af5bd610c2
mleafer <marieleaf@Maries-MacBook-Air-2.local>
2016-06-14 11:06:43 -0400

    feat: add blockchain update template

--- smart_lease/client/main.js
+++ smart_lease/client/main.js
@@ -3,6 +3,14 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
+Session.setDefault('latestBlock', {}); // updated by the 'latestBlock' function in connector.js
+
+Template['blockchainStatus'].helpers({
+	currentBlock: function () {
+		return JSON.stringify(Session.get('latestBlock'), null, 2);
+	}
+});
+
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
@@ -20,6 +28,3 @@ Template.hello.events({
     instance.counter.set(instance.counter.get() + 1);
   },
 });
-
-//new stuff to integrate
-var smartLease = web3.eth.contract([{"constant":false,"inputs":[],"name":"kill","outputs":[],"type":"function"},{"inputs":[],"type":"constructor"}]);
